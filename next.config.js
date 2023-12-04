9b9b6cafd1e7ebf987687331949e4b6c86222466
DevQueen <124140753+oyingrace@users.noreply.github.com>
2023-12-04 17:11:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 }
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
